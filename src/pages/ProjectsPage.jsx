14e52696e5109aecbbd54ce499557d40dc2f25dd
AK <AK@adriennes-mbp.mynetworksettings.com>
2024-04-02 16:33:09 -0500

    added about me subsections

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -44,7 +44,7 @@ function ProjectsPage() {
         <div className="basis-5/6 content-center pt-8 bg-black">
           <div>
             <h1 className="">
-              I'm Anita Haircut
+              I'm DiAnne Cutz
             </h1>
             <h4 className="">
               Hairstylist
@@ -53,44 +53,40 @@ function ProjectsPage() {
           <div className="imageCard flex p-8 justify-center">
             <ImageCard />
           </div>
+          <div>
+            <h3>About Me</h3>
+            <hr className="rule"></hr>
+            <p className="about">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti consectetur dolore laborum nihil cumque, fugiat voluptatum eaque quisquam, eum quas exercitationem, maiores libero ducimus tempora doloribus molestias vel! Doloribus, magnam?</p>
+            <h3>Services</h3>
+            <p>
+              <ul className="list pl-8">
+                <li>Cuts</li>
+                <li>Color</li>
+                <li>Extensions</li>
+                <li>Styling</li>
+              </ul>
+            </p>
+            <div className="">
+              <h3>Reviews</h3>
+              <div className="ml-8">
+                <img className="reviewer" src="/images/face1.png"></img>
+
+                <p className="pt-4">Anita Haircut</p>
+                <p>She's the best!</p>
+                <br />
+
+                <img className="reviewer" src="/images/face2.png"></img>
+
+                <p className="pt-4">Harry Heade</p>
+                <p>No one better than DiAnne</p>
+              </div>
+            </div>
+          </div>
+
         </div>
 
-        {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-8">
-        <ProjectCard
-          title="Travelogue Application"
-          subtitle="Interactive Full-Stack Application"
-          items={travelogueItems}
-        />
-        <ProjectCard
-          title="Wheels of Date Night Application"
-          subtitle="Node.js Application"
-          items={wodItems}
-        />
-        <ProjectCard
-          title="Git-Fit Fitness Tracker"
-          subtitle="MERN Stack Application"
-          items={projectItems}
-        />
-      </div>
-      <h3 className="divider text-4xl pt-8 text-accent">Personal Projects</h3>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-8">
-        <ProjectCard
-          title="Tech-Blog Application"
-          subtitle="A CMS-style blog site"
-          items={techblogItems}
-        />
 
-        <ProjectCard
-          title="Weather Dashboard Application"
-          subtitle="Server-Side API Application"
-          items={weatherItems}
-        />
-        <ProjectCard
-          title="Note Taker"
-          subtitle="Express.js Application"
-          items={noteItems}
-        />
-      </div> */}
+
       </div>
     </>
   );
