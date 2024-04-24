abf3bfc8d8ff879b21fd054a64662bcd49267fe6
AK <AK@adriennes-mbp.mynetworksettings.com>
2024-04-24 08:22:17 -0500

    clean up pages and files

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import ErrorPage from "./pages/ErrorPage";
 import HomePage from "./pages/HomePage";
-import ProjectsPage from "./pages/ProjectsPage";
-import AboutPage from "./pages/AboutPage";
-import TestPage from "./pages/TestPage";
+
 
 // Define the accessible routes, and which components respond to which URL
 const router = createBrowserRouter([
@@ -18,19 +16,7 @@ const router = createBrowserRouter([
         index: true,
         element: <HomePage />,
       },
-      {
-        path: "/About",
-        element: <AboutPage />,
-      },
-      {
-        path: "/Projects",
-        element: <ProjectsPage />,
-      },
-   
-      {
-        path: "Test",
-        element: <TestPage />,
-      },
+
     ],
   },
 ]);
